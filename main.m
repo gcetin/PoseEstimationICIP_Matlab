e5d6ffd938e054f98e7036dfdbc443d28f154740
gcetin <guvencetinkaya@gmail.com>
2025-12-24 00:06:37 +0300

    at home 24.12.2025

--- main.m
+++ main.m
@@ -3,9 +3,9 @@ clc; clear; close all;
 PLOT_FIGURES = false;
 NUM_REPEATS = 5;
 
-folderNameList = {'DataFolder_1920x1080_250_IDEAL_REL_MOTION_TRANS_XYZ_PAN_3D_EIGHT'};
-transErrList = {[0.0, 1.0, 1.0]};
-angErrList = [1.0];
+folderNameList = {'DataFolder_1920x1080_100_NOISY_REL_MOTION_TRANS_XYZ_PAN_3D_EIGHT'};
+transErrList = {[0.0, 0.0, 0.0]};
+angErrList = 0.0;
 
 
 logFileName = PoseEstimationICIP(folderNameList, transErrList, angErrList, PLOT_FIGURES, NUM_REPEATS);
